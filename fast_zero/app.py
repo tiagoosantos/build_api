3c1197af4386cd81d1837dce9938eb67f01ac76f
tiagoosantos <64797098+tiagoosantos@users.noreply.github.com>
2026-06-26 13:51:19 -0300

    correções nos testes

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,11 @@
-from http import HTTPStatus
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 from fastapi import FastAPI
-from fast_zero.schemas import Message, UserSchema, UserPublic, UserDB
+from http import HTTPStatus
 
 app = FastAPI()
 
 database = []  # Simulação de um banco de dados em memória
+
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
@@ -13,7 +14,6 @@ def read_root():
 
 @app.post('/users', response_model=UserPublic, status_code=HTTPStatus.CREATED)
 def create_user(user: UserSchema):
-
     user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
     database.append(user_with_id)
     return user_with_id
--- fast_zero/test_app.py
+++ fast_zero/test_app.py
@@ -1,7 +1,7 @@
 import pytest
-from fastapi.testclient import TestClient
-
 from fast_zero.app import app, database
+from fastapi.testclient import TestClient
+from http import HTTPStatus
 
 client = TestClient(app)
 
@@ -12,26 +12,24 @@ def clear_database():
 
 
 def test_read_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello, World!"}
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello, World!'}
 
 
 def test_create_user():
     response = client.post(
-        "/users",
+        '/users',
         json={
-            "username": "alice",
-            "email": "alice@example.com",
-            "password": "secret123",
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret123',
         },
     )
 
-    assert response.status_code == 201
+    assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
-        "id": 1,
-        "username": "alice",
-        "email": "alice@example.com",
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
     }
-
-
