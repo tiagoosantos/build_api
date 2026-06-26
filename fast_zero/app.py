07ff1edee6a3b2b699e23b162dcf306c55e29c97
tiagoosantos <64797098+tiagoosantos@users.noreply.github.com>
2026-06-26 11:01:22 -0300

    criação de testes e atomações para execução com taskipy

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,19 @@
 from http import HTTPStatus
 from fastapi import FastAPI
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserSchema, UserPublic, UserDB
 
 app = FastAPI()
 
+database = []  # Simulação de um banco de dados em memória
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
+
+
+@app.post('/users', response_model=UserPublic, status_code=HTTPStatus.CREATED)
+def create_user(user: UserSchema):
+
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
--- fast_zero/test_app.py
+++ fast_zero/test_app.py
@@ -1,12 +1,37 @@
+import pytest
 from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+from fast_zero.app import app, database
 
 client = TestClient(app)
 
 
+@pytest.fixture(autouse=True)
+def clear_database():
+    database.clear()
+
+
 def test_read_root():
-    """Testar o endpoint raiz."""
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, World!"}
+
+
+def test_create_user():
+    response = client.post(
+        "/users",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret123",
+        },
+    )
+
+    assert response.status_code == 201
+    assert response.json() == {
+        "id": 1,
+        "username": "alice",
+        "email": "alice@example.com",
+    }
+
+
